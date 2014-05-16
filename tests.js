250c9c694f3943be2595d3d1a3c0aaeb0c624b0f
Caleb Cauthon <calebcauthon@gmail.com>
2014-05-16 12:25:34 -0500

    Both players start with 26 cards.

--- tests.js
+++ tests.js
@@ -1,3 +1,10 @@
-test( "hello test", function() {
-  ok( 1 == "1", "Passed!" );
+
+test( "player 1 has 26 cards to start", function() {
+	var game = new GameOfWar();
+	ok(game.player_1_cards == 26);
+});
+
+test( "player 2 has 26 cards to start", function() {
+	var game = new GameOfWar();
+	ok(game.player_2_cards == 26); 
 });
---
+++ war.js
@@ -0,0 +1,6 @@
+var GameOfWar = function() {
+	return {
+		player_1_cards: 26,
+		player_2_cards: 26	
+	};
+};
