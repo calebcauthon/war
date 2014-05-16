f0edbfd22da530e8798f64ebcc0df62d7719a8ae
Caleb Cauthon <calebcauthon@gmail.com>
2014-05-16 12:33:28 -0500

    Flip the first card.

--- tests.js
+++ tests.js
@@ -1,10 +1,17 @@
 
-test( "player 1 has 26 cards to start", function() {
+test("player 1 has 26 cards to start", function() {
 	var game = new GameOfWar();
-	ok(game.player_1_cards == 26);
+	ok(game.player_1_cards() == 26);
 });
 
-test( "player 2 has 26 cards to start", function() {
+test("player 2 has 26 cards to start", function() {
 	var game = new GameOfWar();
 	ok(game.player_2_cards == 26); 
+});
+
+test("player 1 can flip a card to start", function() {
+	var game = new GameOfWar();
+	game.flip_player_1_card();
+	window.game = game;	
+	ok(game.player_1_cards() == 25);
 });
--- war.js
+++ war.js
@@ -1,6 +1,12 @@
 var GameOfWar = function() {
+	var player_1_cards_in_hand = 26;
+
 	return {
-		player_1_cards: 26,
-		player_2_cards: 26	
+		player_1_cards: function() { return player_1_cards_in_hand; },
+		player_2_cards: 26,
+		flip_player_1_card: function() {
+			player_1_cards_in_hand--;
+			return player_1_cards_in_hand;
+		}
 	};
 };
