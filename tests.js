9120eb2a8dcf273187e78a193533b24358b3d26d
Caleb Cauthon <calebcauthon@gmail.com>
2014-05-16 12:36:16 -0500

    Player 2 can't flip a card to start.

--- tests.js
+++ tests.js
@@ -6,12 +6,17 @@ test("player 1 has 26 cards to start", function() {
 
 test("player 2 has 26 cards to start", function() {
 	var game = new GameOfWar();
-	ok(game.player_2_cards == 26); 
+	ok(game.player_2_cards() == 26); 
 });
 
 test("player 1 can flip a card to start", function() {
 	var game = new GameOfWar();
 	game.flip_player_1_card();
-	window.game = game;	
 	ok(game.player_1_cards() == 25);
+});
+
+test("player 2 cant flip a card to start", function() {
+	var game = new GameOfWar();
+	game.flip_player_2_card();
+	ok(game.player_2_cards() == 26);
 });
--- war.js
+++ war.js
@@ -3,10 +3,11 @@ var GameOfWar = function() {
 
 	return {
 		player_1_cards: function() { return player_1_cards_in_hand; },
-		player_2_cards: 26,
+		player_2_cards: function() { return 26; },
 		flip_player_1_card: function() {
 			player_1_cards_in_hand--;
 			return player_1_cards_in_hand;
-		}
+		},
+		flip_player_2_card: function() {}
 	};
 };
