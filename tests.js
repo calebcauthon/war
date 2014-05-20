77c1d0e30ff9dd7d300fe8a9359d173ecea278d1
Caleb Cauthon <calebcauthon@gmail.com>
2014-05-20 12:02:31 -0500

    Created cards object to hold decks and hands of cards.

--- tests.js
+++ tests.js
@@ -15,8 +15,25 @@ test("player 1 can flip a card to start", function() {
 	ok(game.player_1_cards() == 25);
 });
 
-test("player 2 cant flip a card to start", function() {
+test("player 2 can flip a card to start", function() {
 	var game = new GameOfWar();
 	game.flip_player_2_card();
-	ok(game.player_2_cards() == 26);
+	ok(game.player_2_cards() == 25);
+});
+
+test("cards can be added to a set of cards", function() {
+	var hand = new Cards();
+	hand.add_card({ suit: 'Clubs', rank: 'Ace' });
+	ok(hand.count() == 1);
+});
+
+test("cards transfer to player 1", function() {
+	var full_deck = new Cards();
+	var player_1_cards = new Cards();
+
+	full_deck.add_card({ suit: 'Diamonds', rank: 'Ace' });
+
+	ok(player_1_cards.count == 0);
+	cards.deal(1, player_1_cards());
+	ok(player_1_cards.count == 1);	
 });
--- war.js
+++ war.js
@@ -1,13 +1,30 @@
 var GameOfWar = function() {
 	var player_1_cards_in_hand = 26;
+	var player_2_cards_in_hand = 26;
 
 	return {
 		player_1_cards: function() { return player_1_cards_in_hand; },
-		player_2_cards: function() { return 26; },
+		player_2_cards: function() { return player_2_cards_in_hand; },
 		flip_player_1_card: function() {
 			player_1_cards_in_hand--;
 			return player_1_cards_in_hand;
 		},
-		flip_player_2_card: function() {}
+		flip_player_2_card: function() {
+			player_2_cards_in_hand--;
+			return player_2_cards_in_hand;
+		}
+	};
+};
+
+var Cards = function() {
+	var cards = [];
+
+	return {
+		add_card: function(card) {
+			cards.push(card);	
+		},	
+		count: function() {
+			return cards.length;
+		}
 	};
 };
